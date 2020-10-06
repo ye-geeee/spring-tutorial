805e404611a24475143d02ffe6729977282e4008
ye-geeee <yeaji9683@gmail.com>
2020-10-07 08:48:30 +0900

    Simple Repository Interface and MemoryRepository

---
+++ src/main/java/yegeeee/springtutorial/repository/MemoryMemberRepository.java
@@ -0,0 +1,37 @@
+package yegeeee.springtutorial.repository;
+
+import yegeeee.springtutorial.domain.Member;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new ConcurrentHashMap<>();
+    private static AtomicLong sequence = new AtomicLong();
+
+    @Override
+    public Member save(Member member) {
+        member.setId(sequence.incrementAndGet());
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
